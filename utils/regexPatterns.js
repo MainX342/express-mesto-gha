4b6d0977138073d94ebae7a4d5bfffc8dce7484d
MainX342 <vladimirtitovperm@gmail.com>
2023-08-21 19:37:19 +0500

    fix email regex

--- utils/regexPatterns.js
+++ utils/regexPatterns.js
@@ -1,4 +1,4 @@
 const urlRegex = /^https?:\/\/(?:www\.)?[-a-zA-Z0-9@:%._+~#=]{1,256}\.[a-zA-Z0-9()]{1,6}\b(?:[-a-zA-Z0-9()@:%_+.~#?&/=]*)$/;
-const emailRegex = /^[a-z0-9!#$%&'*+/=?^_`{|}~-]+(?:\.[a-z0-9!#$%&'*+/=?^_`{|}~-]+)*@(?:[a-z0-9](?:[a-z0-9-]*[a-z0-9])?\.)+[a-z0-9](?:[a-z0-9-]*[a-z0-9])?$/;
+const emailRegex = /^\S+@\S+\.\S+$/;
 
 module.exports = { urlRegex, emailRegex };
