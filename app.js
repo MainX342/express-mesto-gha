e54bc65f0189c3bfe04b00218ecc0b58232cf054
MainX342 <vladimirtitovperm@gmail.com>
2023-08-21 21:06:23 +0500

    fix autotest

--- app.js
+++ app.js
@@ -7,21 +7,21 @@ const errorHandler = require('./middlewares/errorHandler');
 const limiter = require('./utils/rateLimiter');
 
 const { PORT = 3000, DB_URL = 'mongodb://127.0.0.1:27017/mestodb' } = process.env;
+const app = express();
 mongoose.connect(DB_URL, {
   useNewUrlParser: true,
   useUnifiedTopology: true,
 });
-
-const app = express();
 
 app.use(cors());
 app.use(helmet());
 app.use(limiter);
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
-app.use(errors());
-app.use(errorHandler);
 
 app.use('/', require('./routes/index'));
+
+app.use(errors());
+app.use(errorHandler);
 
 app.listen(PORT);
